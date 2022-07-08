61249287a4fd12ebde325796d801ee11701a5d52
Yasin Akbas <yasin.kbas12@gmail.com>
2022-07-09 00:09:45 +0300

    add networkKit

---
+++ Kits/CommonKit/Sources/Arguments/Search.swift
@@ -0,0 +1,15 @@
+//
+//  Search.swift
+//  CommonKit
+//
+//  Created by Yasin Akbas on 9.07.2022.
+//  Copyright © 2022 com.yasinkbas.CommonKit. All rights reserved.
+//
+
+import Foundation
+
+public struct Search {
+    public init() {
+        
+    }
+}
---
+++ Kits/NetworkKit/NetworkKit/NetworkKit.h
@@ -0,0 +1,18 @@
+//
+//  NetworkKit.h
+//  NetworkKit
+//
+//  Created by Yasin Akbas on 8.07.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkKit.
+FOUNDATION_EXPORT double NetworkKitVersionNumber;
+
+//! Project version string for NetworkKit.
+FOUNDATION_EXPORT const unsigned char NetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkKit/PublicHeader.h>
+
+
