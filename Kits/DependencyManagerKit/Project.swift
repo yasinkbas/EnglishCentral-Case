d62f3316556ccbff8cbd3c49fc7200fa89ccf177
Yasin Akbas <yasin.kbas12@gmail.com>
2022-07-21 18:16:00 +0300

    add minimum deployment targets

--- Kits/DependencyManagerKit/Project.swift
+++ Kits/DependencyManagerKit/Project.swift
@@ -9,6 +9,7 @@ let project = Project(
             platform: .iOS,
             product: .framework,
             bundleId: "com.yasinkbas.DependencyManagerKit",
+            deploymentTarget: .iOS(targetVersion: "16.0", devices: [.iphone]),
             infoPlist: .default,
             sources: [
                 "Sources/**/*.swift",
--- Kits/MapViewKit/Project.swift
+++ Kits/MapViewKit/Project.swift
@@ -9,6 +9,7 @@ let project = Project(
             platform: .iOS,
             product: .framework,
             bundleId: "com.yasinkbas.MapViewKit",
+            deploymentTarget: .iOS(targetVersion: "16.0", devices: [.iphone]),
             infoPlist: .default,
             sources: [
                 "Sources/**/*.swift",
@@ -24,6 +25,7 @@ let project = Project(
             platform: .iOS,
             product: .unitTests,
             bundleId: "com.yasinkbas.MapViewKitTests",
+            deploymentTarget: .iOS(targetVersion: "16.0", devices: [.iphone]),
             infoPlist: .default,
             sources: [
                 "UnitTests/**"
