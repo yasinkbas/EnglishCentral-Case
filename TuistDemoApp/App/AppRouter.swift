d2c924f44356a100ac6cd03088327f40513ac2c7
Yasin Akbas <yasin.kbas12@gmail.com>
2022-08-05 19:05:00 +0300

    update project name

--- TuistDemoApp/App/AppRouter.swift
+++ TuistDemoApp/App/AppRouter.swift
@@ -1,9 +1,9 @@
 //
 //  AppRouter.swift
-//  EnglishCentral-Case
+//  TuistDemoApp
 //
 //  Created by Yasin Akbas on 10.07.2022.
-//  Copyright © 2022 com.yasinkbas.EnglishCentral-Case. All rights reserved.
+//  Copyright © 2022 com.yasinkbas.TuistDemoApp. All rights reserved.
 //
 
 import UIKit
